2f5bd477f33b812ddd35f1b758a895f216e37e4b
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2022-09-11 23:02:01 -0500

    Modelado de Base de Datos and crud

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as TestRouter } from "./test-component/network";
+export { default as PersonajesRouter } from "./personajes/network";
+export { default as PeliculasRouter } from "./peliculas/network";
+export { default as GenerosRouter } from "./generos/network";
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { TestRouter } from "../components";
+import { PersonajesRouter } from "../components";
+import { PeliculasRouter } from "../components";
+import { GenerosRouter } from "../components";
 
 // cada vez que quiera agregar unaruta nueva,
 // creo el path e importo el componente
-const listRoutes = [["/test", TestRouter]];
+const listRoutes = [
+  ["/test", TestRouter],
+  ["/personajes", PersonajesRouter], 
+  ["/peliculas", PeliculasRouter], 
+  ["/generos", GenerosRouter]
+];
 
 export const routes = (app) => {
   listRoutes.forEach(([path, controller]) => {
